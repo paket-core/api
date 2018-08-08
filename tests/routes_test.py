1afdbef9c3eec43305860395ab4fb134a53e38f0
Israel Levin <israel@paket.global>
2018-08-08 17:35:04 +0300

    Do not use mock db for now

--- tests/routes_test.py
+++ tests/routes_test.py
@@ -8,15 +8,10 @@ import util.logger
 import webserver.validation
 
 import routes
-import tests.db_mockup
 
 LOGGER = util.logger.logging.getLogger('pkt.api.test')
 APP = webserver.setup(routes.BLUEPRINT)
 APP.testing = True
-
-# pylint: disable=invalid-name
-#routes.db = tests.db_mockup
-# pylint: enable=invalid-name
 
 
 class ApiBaseTest(unittest.TestCase):
@@ -286,14 +281,12 @@ class MyPackagesTest(ApiBaseTest):
 
     def test_my_packages(self):
         """Test getting user packages."""
-        util.logger.setup()
         account = self.create_and_setup_new_account()
         LOGGER.info('getting packages for new user: %s', account[0])
         packages = self.call(
             path='my_packages', expected_code=200,
             fail_message='does not get ok status code on valid request', seed=account[1],
             user_pubkey=account[0])['packages']
-        LOGGER.info(111111111111111111111111)
         self.assertTrue(len(packages) == 0)
 
         payment, collateral = 50000000, 100000000
